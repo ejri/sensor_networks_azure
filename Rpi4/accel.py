c85b8af10b1bf2081526ae66f669c2288aa627b0
ejri <ibrahim_10_88@hotmail.com>
2021-01-13 02:29:20 -0800

    rpi4

---
+++ Rpi4/accel.py
@@ -0,0 +1,67 @@
+# connecting the accelerometer GY-521 (MPU6050) to RPi
+# i2cdetect -y 1 to double check if i2c on the pi is confgirured
+# all dep. libraries are in requirements.txt
+# pip3 install -r requirements.txt
+# https://github.com/Arijit1080/mpu6050-with-Raspberry-Pi/blob/master/mpu_6050.py
+
+import smbus
+from mpu6050 import mpu6050
+import time
+import asyncio
+from azure.iot.device.aio import IoTHubDeviceClient
+import json
+
+i2c_address = 0x68
+mpu = mpu6050(i2c_address)
+
+
+async def main():
+
+    conn_str = "HostName=SensorDataRpi4.azure-devices.net;DeviceId=rpi4Tree;SharedAccessKey=cBq5yS4ygdwP4OirmjF49ajrQwjpcCMEmbysIobvk60="
+    device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
+    await device_client.connect()
+
+    while True:
+        print("Temp : " + str(mpu.get_temp()))
+        temp = "{0:0.1f}".format(mpu.get_temp())
+        print()
+        # last_temp= "{0:0.1f}".format(mpu.get_temp())
+
+        accel_data = mpu.get_accel_data()
+        print("Acc X : " + str(accel_data["x"]))
+        print("Acc Y : " + str(accel_data["y"]))
+        print("Acc Z : " + str(accel_data["z"]))
+        print()
+
+        gyro_data = mpu.get_gyro_data()
+        print("Gyro X : " + str(gyro_data["x"]))
+        print("Gyro Y : " + str(gyro_data["y"]))
+        print("Gyro Z : " + str(gyro_data["z"]))
+        print()
+        print("-------------------------------")
+
+        # reduce the number of messages sent, by only sendint out values if
+        # the last value is different eg. temp
+        # if temp != last_temp:
+        # last_temp= temp
+        # json file
+
+        data = {}
+        data["Acc_X"] = "{0:0.1f}".format(accel_data["x"])
+        data["Acc_Y"] = "{0:0.1f}".format(accel_data["y"])
+        data["Acc_Z"] = "{0:0.1f}".format(accel_data["z"])
+        data["Gyro_X"] = "{0:0.1f}".format(gyro_data["x"])
+        data["Gyro_Y"] = "{0:0.1f}".format(gyro_data["y"])
+        data["Gyro_Z"] = "{0:0.1f}".format(gyro_data["z"])
+        data["Temprature"] = temp
+        json_body = json.dumps(data)
+        print("Sending message: ", json_body)
+        await device_client.send_message(json_body)
+
+        time.sleep(5)
+
+    await device_client.disconnect()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
