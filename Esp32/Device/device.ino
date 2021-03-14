54b2f41aa92adc83e072781ac904b7a534f93693
ibrahim <ibrahimelchami@gmail.com>
2021-03-14 05:15:47 -0700

    multiple IMUs

--- Esp32/Device/device.ino
+++ Esp32/Device/device.ino
@@ -2,6 +2,31 @@
 // This code: connecting an accelerometer to esp32. The raw accelerometer data is sent to IoT Hub.
 // IoT Hub connection, and device provisioning, etc. :
 
+// SimpleMQTT_esp32_azure_iothub.ino
+// downloads/ubc_5G/sensor_networks_azure/
+
+// Azure ESP32 IoT DevKit Get Started
+// https://docs.microsoft.com/en-us/samples/azure-samples/esp32-iot-devkit-get-started/sample/
+
+/**
+ * A simple Azure IoT example for sending telemetry to Iot Hub.
+ 
+ original code from:
+ https://github.com/VSChina/ESP32_AzureIoT_Arduino
+
+IoT Hub has to be Standard Tier, Basic Tier creates a lot of issues
+ 
+ */
+
+// TCA9548A I2C Switch between:
+// adafruit LSM9DS1 at address, on TCA9548's channel 0
+// GY521-MPU 6050 at address, on TCA9548's channel 1
+// TCA9548 module I2C address:  0x70
+//
+
+//======================================================
+//I2C Mutiplexing using TCA9548A at I2C address 0x70
+//======================================================
 
 // GY-521 / mpu6050 on esp32
 // connections: 5v, GND, SDA, SCL, on sensor to 5v, GND, SDA, SCL on esp32 respectively
@@ -23,18 +48,61 @@ const int MPU = 0x68;
 int16_t aX, aY, aZ, gX, gY, gZ;
 //////////////////////////// Accelerometer ////////////////////
 
-#define INTERVAL 10000 //10seconds intervals sending messages
+#include <Adafruit_LSM9DS1.h>
+#include <Adafruit_Sensor.h> // not used in this demo but required!
+
+//////////////////////////// LSM9DS1 ////////////////////
+Adafruit_LSM9DS1 lsm_1 = Adafruit_LSM9DS1(1);
+
+#define LSM9DS1_SCK A5
+#define LSM9DS1_MISO 12
+#define LSM9DS1_MOSI A4
+#define LSM9DS1_XGCS 6
+#define LSM9DS1_MCS 5
+
+void TCA9548A(uint8_t bus)
+{
+  Wire.beginTransmission(0x70);
+  Wire.write(1 << bus);
+  Wire.endTransmission();
+}
+
+void setupSensor()
+{
+  // 1.) Set the accelerometer range
+  lsm_1.setupAccel(lsm_1.LSM9DS1_ACCELRANGE_2G);
+  //lsm_1.setupAccel(lsm_1.LSM9DS1_ACCELRANGE_4G);
+  //lsm_1.setupAccel(lsm_1.LSM9DS1_ACCELRANGE_8G);
+  //lsm_1.setupAccel(lsm_1.LSM9DS1_ACCELRANGE_16G);
+
+  // 2.) Set the magnetometer sensitivity
+  lsm_1.setupMag(lsm_1.LSM9DS1_MAGGAIN_4GAUSS);
+  //lsm_1.setupMag(lsm_1.LSM9DS1_MAGGAIN_8GAUSS);
+  //lsm_1.setupMag(lsm_1.LSM9DS1_MAGGAIN_12GAUSS);
+  //lsm_1.setupMag(lsm_1.LSM9DS1_MAGGAIN_16GAUSS);
+
+  // 3.) Setup the gyroscope
+  lsm_1.setupGyro(lsm_1.LSM9DS1_GYROSCALE_245DPS);
+  //lsm_1.setupGyro(lsm_1.LSM9DS1_GYROSCALE_500DPS);
+  //lsm_1.setupGyro(lsm_1.LSM9DS1_GYROSCALE_2000DPS)a
+}
+
+//=============================================================================
+
+#define INTERVAL 15000 //10seconds intervals sending messages
 #define MESSAGE_MAX_LEN 256
 // Please input the SSID and password of WiFi
-const char *ssid = "";
-const char *password = "";
+const char *ssid = "Liverpool FC";
+const char *password = "6042140214";
 
+/// Primaty Connection String///
 /*String containing Hostname, Device Id & Device Key in the format:                         */
 /*  "HostName=<host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"                */
 /*  "HostName=<host_name>;DeviceId=<device_id>;SharedAccessSignature=<device_sas_token>"    */
-static const char *connectionString = 
+static const char *connectionString = "HostName=GreenSpaces.azure-devices.net;DeviceId=GreenSpaceTree1;SharedAccessKey=Y4IN6au/gQSVpObThi7uJ1m+y4b823YgadkXYbK6vx0=";
 //const char *messageData = "{\"messageId\":%d, \"Temperature\":%f, \"Humidity\":%f}";
-const char *messageData = "{\"messageId\":%d, \"aX\":%d, \"aY\":%d, \"aZ\":%d, \"gX\":%d, \"gY\":%d, \"gZ\":%d}";
+const char *DeviceID = "GreenSpaceTree1";
+const char *messageData = "{\"DeviceID\":\"GreenSpaceTree1\" ,\"messageId\":%d, \"aX\":%d, \"aY\":%d, \"aZ\":%d, \"gX\":%d, \"gY\":%d, \"gZ\":%d}";
 static bool hasIoTHub = false;
 static bool hasWifi = false;
 int messageCount = 1;
@@ -136,12 +204,32 @@ void setup()
   randomSeed(analogRead(0));
   //send_interval_ms = millis();   // moved to after adding the accelerometer
 
-  //Setup the registers of the MPU-6050
   Wire.begin();
+
+  /* Initialise the 1st sensor */
+  Serial.println("LSM9DS1 data read demo");
+
+  // lsm9ds1 i2c at the multiplexer channel 1. check if sensor is connected
+  TCA9548A(0);
+  if (!lsm_1.begin())
+  {
+    Serial.println("Oops ... unable to initialize the LSM9DS1. Check your wiring!");
+    while (1)
+      ;
+  }
+  Serial.println("Found LSM9DS1 9DOF");
+
+  // sensor connected. initiate sensor
+  TCA9548A(0);
+  setupSensor();
+
+  // mpu6050 i2c at the multiplexer channel 1.
+  TCA9548A(1);
   Wire.beginTransmission(MPU);
   Wire.write(0x6B);
   Wire.write(0);
   Wire.endTransmission(true);
+  delay(10);
 
   //// start count///
   send_interval_ms = millis();
@@ -155,7 +243,50 @@ void loop()
         (int)(millis() - send_interval_ms) >= INTERVAL)
     {
 
+      TCA9548A(0);
+      lsm_1.read(); /* ask it to read in the data */
+
+      /* Get a new sensor event */
+      TCA9548A(0);
+      sensors_event_t a, m, g, temp;
+      TCA9548A(0);
+      lsm_1.getEvent(&a, &m, &g, &temp);
+
+      Serial.print("Accel X: ");
+      Serial.print(a.acceleration.x);
+      Serial.print(" m/s^2");
+      Serial.print("\tY: ");
+      Serial.print(a.acceleration.y);
+      Serial.print(" m/s^2 ");
+      Serial.print("\tZ: ");
+      Serial.print(a.acceleration.z);
+      Serial.println(" m/s^2 ");
+
+      Serial.print("Mag X: ");
+      Serial.print(m.magnetic.x);
+      Serial.print(" uT");
+      Serial.print("\tY: ");
+      Serial.print(m.magnetic.y);
+      Serial.print(" uT");
+      Serial.print("\tZ: ");
+      Serial.print(m.magnetic.z);
+      Serial.println(" uT");
+
+      Serial.print("Gyro X: ");
+      Serial.print(g.gyro.x);
+      Serial.print(" rad/s");
+      Serial.print("\tY: ");
+      Serial.print(g.gyro.y);
+      Serial.print(" rad/s");
+      Serial.print("\tZ: ");
+      Serial.print(g.gyro.z);
+      Serial.println(" rad/s");
+
+      Serial.println();
+      delay(10);
+
       // get raw accelerometer data /////
+      TCA9548A(1);
       Wire.beginTransmission(MPU);
       Wire.write(0x3B);
       Wire.endTransmission(false);
@@ -178,14 +309,19 @@ void loop()
     */
 
       // Serial print acceleromter data
-      /*Serial.print(aX);
-    Serial.print(","); Serial.print(aY);
-    Serial.print(","); Serial.print(aZ); 
-    Serial.print(","); Serial.print(gX);
-    Serial.print(","); Serial.print(gY);
-    Serial.print(","); Serial.println(gZ);
-    */
-      delay(333);
+      Serial.print(aX);
+      Serial.print(",");
+      Serial.print(aY);
+      Serial.print(",");
+      Serial.print(aZ);
+      Serial.print(",");
+      Serial.print(gX);
+      Serial.print(",");
+      Serial.print(gY);
+      Serial.print(",");
+      Serial.println(gZ);
+
+      delay(2000);
       ////  raw accelerometer data //////
 
       // generate random temperature, humidity data
@@ -205,5 +341,5 @@ void loop()
       Esp32MQTTClient_Check();
     }
   }
-  delay(10);
+  delay(5000);
 }
